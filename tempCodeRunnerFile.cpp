ba404069119ed5b604794b9c10a8e0d8f66ce011
Radhika Agarwal <radhikaag18@gmail.com>
2026-07-04 12:15:56 +0530

    threadtest moved outside

--- tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-    map<string, vector<int>> gpaIndex = buildIndex<string>(table.rows, 2);   // column 2 = gpa
-
-    for (auto& pair : gpaIndex) {
-        cout << pair.first << " -> rows: ";
-        for (int pos : pair.second) {
-            cout << pos << " ";
-        }
-        cout << "\n";
-    }
